b3b18fda2484700eb4a913037a7eb44ba7473743
Muslimbello <yuusufyusasif@gmail.com>
2023-12-06 01:37:18 +0100

    Finished Day1 of 100daysofcode

---
+++ Day1/band_name_gen.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+city = str(input("What's the name of the city you grew up in?\n"))
+pet_name = str(input("What is your pet name?\n"))
+print(f" Your band name could be {city} {pet_name }")
